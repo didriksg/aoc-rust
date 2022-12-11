017f343f5cf824f8782d7a4b9da4063603bdb7c9
Didrik <kiddid96@gmail.com>
2022-12-11 22:57:06 +0100

    Beautify day 5

--- day_5/src/main.rs
+++ day_5/src/main.rs
@@ -4,22 +4,25 @@ use regex::Regex;
 
 
 fn parse_board(board: &str) -> Vec<Vec<&str>> {
+    // Split "board" strings into rows.
     let board_vector: Vec<&str> = board.split("\n").collect();
-    let board_size = board_vector.len();
-    let board_values = &board_vector[0..board_size - 1];
-    let board_size = (board_vector[0].len() + 1) / 4;
 
+    // All but last row is information about the stacks.
+    let board_values = board_vector[0..board_vector.len() - 1].to_owned();
+    let number_of_stacks = (board_vector[0].len() + 1) / 4;
+
+    // Initiate stacks.
     let mut stacks: Vec<Vec<&str>> = Vec::new();
-    for _ in 0..board_size {
+    for _ in 0..number_of_stacks {
         stacks.push(Vec::new());
     }
 
+    // Insert row values into its associated stack.
     let regex_pattern: Regex = Regex::new(r"(\w+\s*)").unwrap();
-
-    for bab in board_values {
-        let values = regex_pattern.find_iter(bab);
-        for value in values {
-            let value_pos = value.start() / 4;
+    for board_value in board_values {
+        for value in regex_pattern.find_iter(board_value) {
+            // 1-index of position can be divided by 4 to get the correct stack.
+            let value_pos = (value.start() + 1) / 4;
             let value_val = value.as_str();
 
             stacks[value_pos].insert(0, value_val);
@@ -29,21 +32,27 @@ fn parse_board(board: &str) -> Vec<Vec<&str>> {
     return stacks;
 }
 
+/// Moves and reverses order.
 fn move_push_pop(board: &mut Vec<Vec<&str>>, move_count: usize, move_from_index: usize, move_to_index: usize) {
+    // You achieve the same by reversing the order of the string slice in move_collect.
     for _ in 0..move_count {
         let move_val = board[move_from_index].pop().unwrap();
         board[move_to_index].push(move_val);
     }
+
 }
 
+/// Moves and keeps order.
 fn move_collect(board: &mut Vec<Vec<&str>>, move_count: usize, move_from_index: usize, move_to_index: usize) {
     let drain_amount = board[move_from_index].len() - move_count;
     let mut move_values: Vec<&str> = board[move_from_index].drain(drain_amount..).collect();
+
     board[move_to_index].append(&mut move_values);
 }
 
+/// Collects the top elements of all stacks into a string.
 fn generate_last_item_string(board: Vec<Vec<&str>>) -> String {
-    let mut value_string: String = "".parse().unwrap();
+    let mut value_string: String = String::new();
     for stack in &*board {
         value_string.push_str(stack.last().unwrap());
     }
@@ -51,10 +60,11 @@ fn generate_last_item_string(board: Vec<Vec<&str>>) -> String {
     return value_string;
 }
 
-
-fn process_instructions_stack(board: &Vec<Vec<&str>>, instruction_set: &str) {
+/// Main function to process the instruction strings.
+fn process_instructions_stack(board: Vec<Vec<&str>>, instruction_set: &str) {
     let instruction_pattern: Regex = Regex::new(r"move (\d+) from (\d+) to (\d+)").unwrap();
 
+    // Keep two separate boards for the two different parts.
     let mut board_push_pop = board.clone();
     let mut board_collect = board.clone();
 
@@ -83,7 +93,7 @@ fn part_1(input_lines: &String) {
     let board = parse_board(split_values[0]);
     let info_lines = split_values[1];
 
-    process_instructions_stack(&board, info_lines);
+    process_instructions_stack(board, info_lines);
 }
 
 
