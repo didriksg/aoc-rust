e818de5e36ac28755667d735961128921082ab4e
Didrik <kiddid96@gmail.com>
2022-12-11 23:27:35 +0100

    Small fix for day 6

--- day_6/src/main.rs
+++ day_6/src/main.rs
@@ -2,35 +2,31 @@ use std::collections::HashSet;
 use utility_library::read_file;
 
 fn solver(input_signal: &str, value_needed: usize) -> usize {
-    let mut start_pointer = 0;
-    let mut end_pointer = value_needed;
+    let mut pointer = 0;
     let str_length = input_signal.len();
 
-    let mut combination_found = false;
-    while !combination_found {
-        let bab = input_signal[start_pointer..end_pointer].chars();
-        let current_signal: HashSet<char> = HashSet::from_iter(bab);
-        combination_found = current_signal.len() == value_needed;
+    loop {
+        let end_point = pointer + value_needed;
+        let current_signal: HashSet<char> = HashSet::from_iter(input_signal[pointer..end_point].chars());
 
-        if end_pointer > str_length {
+        if current_signal.len() == value_needed || end_point >= str_length {
             break;
         }
 
-        start_pointer += 1;
-        end_pointer += 1;
+        pointer += 1;
     }
-    return end_pointer - 1;
+    return pointer + value_needed;
 }
 
 
 fn main() {
-    let lines = read_file("/home/didrik/IdeaProjects/aoc/day_6/input.txt");
+    let lines = read_file("/home/didrik/git/aoc-rust/day_6/input.txt");
 
     for line in lines.split("\n") {
         let part_1_pos = solver(line, 4);
         let part_2_pos = solver(line, 14);
 
         println!("Part-1: {part_1_pos}");
-        println!("Part-2: {part_2_pos}");
+        println!("Part-2: {part_2_pos}\n");
     }
 }
